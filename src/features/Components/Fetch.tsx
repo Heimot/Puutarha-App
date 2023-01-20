250a1ef8a7cad720bcbdf9079f2e6ce3b1fa11df
Heimot <46439240+Heimot@users.noreply.github.com>
2023-01-20 16:49:12 +0200

    Changeable fetch.

---
+++ src/features/Components/Fetch.tsx
@@ -0,0 +1,44 @@
+interface Props {
+    urlHost: string | undefined;
+    urlPath: string;
+    urlQuery?: string;
+    urlMethod: string;
+    urlHeaders: string;
+    urlBody?: Object;
+}
+
+const FetchData = async ({ urlHost, urlPath, urlQuery, urlMethod, urlHeaders, urlBody }: Props) => {
+    let uq = urlQuery;
+    if (uq === undefined) uq = '';
+    let url = urlHost + urlPath + uq;
+    let body = JSON.stringify(urlBody);
+    let headersData = {};
+    if (urlHeaders === "NoAuth") {
+        headersData = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    } else if (urlHeaders === "Auth") {
+        headersData = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
+    }
+
+    if (!urlHost) return;
+    let data = await fetch(url, {
+        method: urlMethod,
+        headers: headersData,
+        body
+    })
+    if(!data.ok) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+        window.location.href = '/'
+        return;
+    }
+    return data.json();
+}
+
+export default FetchData;
