e01e0fc60407b6c983fa58fe44ca35f70ba3e39f
Heimot <46439240+Heimot@users.noreply.github.com>
2023-01-20 16:47:47 +0200

    Added routing and automatic settings fetching.

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router';
+import Login from './features/Login/Login';
+import Main from './features/MainMenu/Main';
+import NavigationBar from './features/Navigation/NavigationBar';
 
-function App() {
+import { useSelector, useDispatch } from 'react-redux';
+import { State } from './app/redux/store';
+import FetchData from './features/Components/Fetch';
+import { bindActionCreators } from '@reduxjs/toolkit';
+import * as actionCreators from './app/redux/actions';
+import dayjs from 'dayjs';
+import Settings from './features/Settings/Settings';
+import Calendar from './features/Calendar/Calendar';
+
+const App = () => {
+  const { userData } = useSelector((state: State) => state.data);
+
+  const dispatch = useDispatch();
+  const { setUserData, setStatus, setState, setLocation, setPDF, setChosenDate } = bindActionCreators(actionCreators, dispatch);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let auth = localStorage.getItem('token');
+    let userId = localStorage.getItem('userId');
+    if (!auth || !userId) return navigate('/');
+    let url = process.env.REACT_APP_API_URL;
+    const getData = async () => {
+      let data = userData;
+      if (data === null) {
+        data = await FetchData({ urlHost: url, urlPath: '/auth/get_my_user_data', urlMethod: 'GET', urlHeaders: 'Auth', urlQuery: `?currentUserId=${userId}` });
+        setUserData(data.result);
+      }
+      if (data) {
+        let applicationSettings = await FetchData({ urlHost: url, urlPath: '/settings/get_settings', urlMethod: 'GET', urlHeaders: 'Auth', urlQuery: `?currentUserId=${userId}` });
+        setStatus(applicationSettings.status);
+        setState(applicationSettings.state);
+        setLocation(applicationSettings.location);
+        setPDF(applicationSettings.pdf);
+
+        setChosenDate(dayjs().toString())
+        let path = window.location.pathname;
+        if (path !== '/') {
+          navigate(path);
+        } else {
+          navigate('/dashboard');
+        }
+      }
+    }
+    getData();
+  }, [window.location.pathname])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      {window.location.pathname.includes('/dashboard')
+        ?
+        <NavigationBar />
+        :
+        null}
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/dashboard' element={<Main />} />
+        <Route path='/dashboard/settings' element={<Settings />} />
+        <Route path='/dashboard/calendar' element={<Calendar />} />
+      </Routes>
     </div>
   );
 }
