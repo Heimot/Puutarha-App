2b93414fd1932a2d3c495be1a6dcde5a6207b902
Heimot <46439240+Heimot@users.noreply.github.com>
2023-01-23 16:07:44 +0200

    Mapped table data.

---
+++ src/features/MainMenu/MainTableData.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { Order, Products } from './Model'
+import { Tr, Td } from 'react-super-responsive-table'
+import { Button, TextField } from '@mui/material';
+import FetchData from '../Components/Fetch';
+
+interface Props {
+    product: Products;
+    updateOrder: (nextState: string, pickedAmount: number) => void;
+}
+
+const MainTableData: React.FC<Props> = ({ product, updateOrder }) => {
+    const [pickedAmount, setPickedAmount] = useState<number>(product.amountToDeliver);
+
+    const nextState = async () => {
+        let userId = localStorage.getItem('userId');
+        let url = process.env.REACT_APP_API_URL;
+        let body = {
+            currentUserId: userId,
+            _id: product._id,
+            amountToDeliver: pickedAmount,
+            cards: ["777000"]
+        }
+        await FetchData({ urlHost: url, urlPath: '/products/change_products_state', urlMethod: 'POST', urlHeaders: 'Auth', urlBody: body });
+        updateOrder(product.state.nextState, pickedAmount);
+    }
+
+    return (
+        <Tr>
+            <Td>{product.flower.name}</Td>
+            <Td>{product.amount}</Td>
+            <Td>{product.location.location}</Td>
+            <Td>{product.information}</Td>
+            <Td style={{ padding: '5px' }}>
+                <Button
+                    variant="contained"
+                    size='small'
+                    sx={{ fontSize: 15, whiteSpace: 'wrap', textTransform: 'none' }}
+                    style={{ maxWidth: '150px', minWidth: '150px', backgroundColor: product.state.color, color: product.state.fontcolor }}
+                    onClick={() => nextState()}
+                >
+                    {product.state.state}
+                </Button>
+            </Td>
+            <Td>
+                <TextField
+                    sx={{ padding: '5px' }}
+                    inputProps={{ style: { textAlign: 'center' } }}
+                    type="number"
+                    value={pickedAmount}
+                    onChange={(e: any) => setPickedAmount(e.target.value)}
+                />
+            </Td>
+        </Tr>
+    )
+}
+
+export default MainTableData
