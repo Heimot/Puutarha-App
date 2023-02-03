c5230edaf809974ffcca0162a8ec6641004510b1
Heimot <46439240+Heimot@users.noreply.github.com>
2023-02-03 16:30:21 +0200

    Settings page roles added.

---
+++ src/features/Settings/SettingsRoles.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Grid from '@mui/material/Unstable_Grid2';
+
+const SettingsRoles = () => {
+    return (
+        <Grid container sx={{ width: '100%' }} justifyContent={'center'} alignItems={'center'}>
+            <div>Muokkaa rooleja</div>
+        </Grid>
+    )
+}
+
+export default SettingsRoles
