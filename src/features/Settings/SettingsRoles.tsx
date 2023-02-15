092f85b518253b5c4bf3991215ca99de28bdad6c
Heimot <46439240+Heimot@users.noreply.github.com>
2023-02-15 17:05:33 +0200

    You can now create roles.

--- src/features/Settings/SettingsRoles.tsx
+++ src/features/Settings/SettingsRoles.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { TextField, Button, Typography, Select, MenuItem, FormControl, InputLabel, Box, FormGroup, FormControlLabel, Switch } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 
@@ -7,6 +7,7 @@ import Paper from '@mui/material/Paper';
 
 import { Role } from '../../Model';
 import FetchData from '../Components/Fetch';
+import Roles from './Roles';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,33 +15,80 @@ const Item = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-    border: theme.palette.mode === 'dark' ? 'none' : 'solid 1px black'
+    border: 'solid 1px black'
 }));
 
 const SettingsRoles = () => {
-    const [roles, setRoles] = useState<Role[]>();
+    const [roles, setRoles] = useState<Role[]>([]);
     const [role, setRole] = useState<string>('');
+    const [isDefault, setIsDefault] = useState<boolean>(false);
+
+    const newRole = useRef<any>();
+
+    const [chosenPermissions, setChosenPermissions] = useState<string[]>(['/auth/get_my_user_data']);
 
     useEffect(() => {
         const getRole = async () => {
             let userId = localStorage.getItem('userId');
             let url = process.env.REACT_APP_API_URL;
             const fetchRoles = await FetchData({ urlHost: url, urlPath: '/roles/get_roles', urlMethod: 'GET', urlHeaders: 'Auth', urlQuery: `?currentUserId=${userId}` });
-
-            const defaultRole = fetchRoles.result.filter((role: Role) => {
-                return role.default;
-            });
-
             setRoles(fetchRoles.result);
-            setRole(defaultRole[0]._id);
         }
         getRole();
 
         return () => {
-            setRoles(undefined);
+            setRoles([]);
             setRole('');
         }
     }, [])
+
+    const updateRole = async () => {
+        if (role === '') {
+            if (newRole.current.value === '') return alert('Et voi luoda roolia jolla ei ole nimeä.');
+            let userId = localStorage.getItem('userId');
+            let url = process.env.REACT_APP_API_URL;
+            let body = {
+                currentUserId: userId,
+                role: newRole.current.value,
+                rights: chosenPermissions,
+                default: isDefault
+            }
+            const createRole = await FetchData({ urlHost: url, urlPath: '/roles/create_role', urlMethod: 'POST', urlHeaders: 'Auth', urlBody: body });
+            setRoles(prevState => [...prevState, createRole.result]);
+            setRole(createRole.result._id)
+        } else {
+
+        }
+    }
+
+    const chosenRole = (id: string) => {
+        if (id !== '') {
+            const cRole = roles?.filter((item) => {
+                return item._id === id
+            })[0];
+            setChosenPermissions(cRole?.rights as string[]);
+            setIsDefault(cRole?.default as boolean);
+            newRole.current.value = cRole?.role;
+        } else {
+            setChosenPermissions(['/auth/get_my_user_data']);
+            setIsDefault(false);
+            newRole.current.value = '';
+        }
+    }
+
+    useEffect(() => {
+        console.log(chosenPermissions)
+    }, [chosenPermissions])
+
+    const addRole = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) {
+            setChosenPermissions(prevState => [...prevState, e.target.value]);
+        } else {
+            setChosenPermissions(prevState => [...prevState.filter((perm) => {
+                return perm !== e.target.value;
+            })])
+        }
+    }
 
     return (
         <Grid container sx={{ width: '100%', padding: '10px' }} justifyContent={'center'} alignItems={'center'}>
@@ -49,11 +97,12 @@ const SettingsRoles = () => {
                     <Box sx={{ maxWidth: '500px' }}>
                         <Box>
                             <Typography>
-                                Muokkaa roolia
+                                {'Muokkaa roolia (Jätä tyhjäksi mikäli luot uutta roolia)'}
                             </Typography>
                             <FormControl fullWidth sx={{ marginTop: '5px' }}>
                                 <InputLabel id='roles-label'>Rooli</InputLabel>
-                                <Select label='Rooli' labelId='roles-label' value={role} onChange={(e: any) => setRole(e.target.value)}>
+                                <Select label='Rooli' labelId='roles-label' value={role} onChange={(e) => { setRole(e.target.value); chosenRole(e.target.value); }}>
+                                    <MenuItem value=''>{'Tyhjä (Uusi rooli)'}</MenuItem>
                                     {
                                         roles?.map((role) => (
                                             <MenuItem key={role?._id} value={role?._id}>{role?.role}</MenuItem>
@@ -63,24 +112,49 @@ const SettingsRoles = () => {
                             </FormControl>
                         </Box>
                         <Box sx={{ marginTop: '20px' }}>
-                            <Typography>
-                                Luo uusi rooli
-                            </Typography>
-                            <TextField sx={{ marginTop: '5px' }} />
+                            <Typography>Roolin nimi</Typography>
+                            <TextField inputRef={newRole} fullWidth sx={{ marginTop: '5px' }} />
                         </Box>
-                        <Button>Luo rooli</Button>
+                        <FormControlLabel
+                            sx={{ display: 'flex', textAlign: 'left', justifyContent: 'center' }}
+                            control={
+                                <Switch
+                                    checked={isDefault}
+                                    onChange={(e) => setIsDefault(e.target.checked)}
+                                />
+                            }
+                            label='Default'
+                        />
+                        <Button onClick={() => updateRole()}>{role === '' ? 'Luo rooli' : 'Päivitä roolia'}</Button>
                     </Box>
                     <FormGroup>
-                        <Grid container>
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        onChange={(e) => console.log(e.target.checked)}
-                                    />
-                                }
-                                label="Voi nähdä kaikki käyttäjät"
-                            />
-                        </Grid>
+                        {
+                            Roles.map((role) => {
+                                return (
+                                    <Item key={role.name} sx={{ marginBottom: '20px' }}>
+                                        <Typography>{role.name}</Typography>
+                                        <Grid container>
+                                            {role.permissions.map((perm, index) => (
+                                                <Grid xs={12} md={4} lg={3} key={index}>
+                                                    <FormControlLabel
+                                                        sx={{ display: 'flex', textAlign: 'left' }}
+                                                        control={
+                                                            <Switch
+                                                                value={perm.path}
+                                                                disabled={perm?.disabled ? true : false}
+                                                                checked={perm?.disabled ? true : chosenPermissions?.includes(perm.path) || chosenPermissions?.includes('*')}
+                                                                onChange={(e) => addRole(e)}
+                                                            />
+                                                        }
+                                                        label={perm.label}
+                                                    />
+                                                </Grid>
+                                            ))}
+                                        </Grid>
+                                    </Item>
+                                )
+                            })
+                        }
                     </FormGroup>
                 </Item>
             </Grid>
