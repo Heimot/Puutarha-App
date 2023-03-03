5febdd6994dbc0f5d307e398acdaecddf58343a8
Heimot <46439240+Heimot@users.noreply.github.com>
2023-03-03 16:28:50 +0200

    Added auth checks to menus.

--- src/features/Settings/SettingsNav.tsx
+++ src/features/Settings/SettingsNav.tsx
@@ -17,21 +17,25 @@ import SearchIcon from '@mui/icons-material/Search';
 import RememberMeIcon from '@mui/icons-material/RememberMe';
 import InventoryIcon from '@mui/icons-material/Inventory';
 import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { State } from '../../app/redux/store';
+import { useSelector } from 'react-redux';
 
 const data = [
-    { icon: <LanguageIcon />, label: 'Kieli', id: 'language' },
+    { icon: <SettingsIcon />, label: 'Omat asetukset', id: 'personalsettings' },
+    { icon: <LanguageIcon />, label: 'Kieli', id: 'language' }
 ];
 
 const adminData = [
-    { icon: <AccountCircleIcon />, label: 'Muokkaa käyttäjiä', id: 'users' },
-    { icon: <RememberMeIcon />, label: 'Muokkaa rooleja', id: 'roles' },
-    { icon: <TuneIcon />, label: 'RFID', id: 'rfid' },
-    { icon: <InventoryIcon />, label: 'Muokkaa tuotteen tiloja', id: 'state' },
-    { icon: <PrecisionManufacturingIcon />, label: 'Muokkaa tilauksen tiloja', id: 'status' },
-    { icon: <ExploreIcon />, label: 'Muokkaa keräyspaikkoja', id: 'location' },
-    { icon: <SearchIcon />, label: 'Muokkaa kauppoja ja kukkia', id: 'autofill' },
-    { icon: <LabelIcon />, label: 'Muokkaa tarroja', id: 'stickers' },
-    { icon: <ListAltIcon />, label: 'Logit', id: 'logs' },
+    { icon: <AccountCircleIcon />, label: 'Muokkaa käyttäjiä', id: 'users', hidden: '/auth/sign_up' },
+    { icon: <RememberMeIcon />, label: 'Muokkaa rooleja', id: 'roles', hidden: '/roles/create_role' },
+    { icon: <TuneIcon />, label: 'RFID', id: 'rfid', hidden: '/card/add_card' },
+    { icon: <InventoryIcon />, label: 'Muokkaa tuotteen tiloja', id: 'state', hidden: '/state/create_state' },
+    { icon: <PrecisionManufacturingIcon />, label: 'Muokkaa tilauksen tiloja', id: 'status', hidden: '/status/create_status' },
+    { icon: <ExploreIcon />, label: 'Muokkaa keräyspaikkoja', id: 'location', hidden: '/location/create_location' },
+    { icon: <SearchIcon />, label: 'Muokkaa kauppoja ja kukkia', id: 'autofill', hidden: '/names/create_name' },
+    { icon: <LabelIcon />, label: 'Muokkaa tarroja', id: 'stickers', hidden: '/pdf/create_pdf' },
+    { icon: <ListAltIcon />, label: 'Logit', id: 'logs', hidden: '/log/get_logs' },
 ];
 
 const SettingsNavBar = styled(List)<{ component?: React.ElementType }>({
@@ -56,6 +60,8 @@ interface Props {
 const SettingsNav: React.FC<Props> = ({ page, setPage }) => {
     const [open, setOpen] = useState(true);
     const [adminOpen, setAdminOpen] = useState(true);
+
+    const { userData } = useSelector((state: State) => state.data);
 
     return (
         <Paper elevation={0} sx={{ maxWidth: '100%', bgcolor: 'rgba(71, 98, 130, 0.2)' }}>
@@ -84,7 +90,7 @@ const SettingsNav: React.FC<Props> = ({ page, setPage }) => {
                                 lineHeight: '20px',
                                 mb: '2px',
                             }}
-                            secondary="Kieli"
+                            secondary="Kieli, Omat asetukset"
                             secondaryTypographyProps={{
                                 noWrap: true,
                                 fontSize: 12,
@@ -167,7 +173,7 @@ const SettingsNav: React.FC<Props> = ({ page, setPage }) => {
                         adminData.map((item) => (
                             <ListItemButton
                                 key={item.label}
-                                sx={{ py: 0, minHeight: 32 }}
+                                sx={{ py: 0, minHeight: 32, display: userData?.role?.rights.includes('*') || userData?.role?.rights.includes(item?.hidden) ? 'flex' : 'none' }}
                                 onClick={() => setPage(item.id)}
                                 selected={page === item.id}
                             >
